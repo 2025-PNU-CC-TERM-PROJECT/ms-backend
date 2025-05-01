972f935eb68fc36b12b461aff801f3523bd78c9b
yeseul-kim01 <yesul0718@naver.com>
2025-05-01 12:14:33 +0900

    Initial commit

---
+++ src/main/java/org/example/term_pj/TermPjApplication.java
@@ -0,0 +1,13 @@
+package org.example.term_pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermPjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TermPjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/term_pj/TermPjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.term_pj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermPjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
